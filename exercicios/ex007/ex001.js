bcd00077b2138ed338671bcf5dd89ebeb7a21556
Wallerson Martins <106995658+WallersonMartins@users.noreply.github.com>
2022-06-15 08:13:51 -0300

    att dia 14/06

---
+++ exercicios/ex007/ex001.js
@@ -0,0 +1,9 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60.0) {
+    console.log(`Voce foi multado`)
+} else {
+    console.log(`Mantenha a velocidade`)
+}
+console.log(`Dirija sempre com cinto de segurança sua velocidade foi de ${vel}km/h`)
+
